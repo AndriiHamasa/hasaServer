b600e6b5ee502e2c8b372e29dd743a4c954b6ed0
AndriiHamasa <andrii.khamaza@nure.ua>
2023-11-06 10:59:36 +0300

    change port

--- server.js
+++ server.js
@@ -4,16 +4,13 @@ import mongoose from "mongoose";
 import { app } from "./app.js";
 import "dotenv/config";
 
-const { DB_HOST } = process.env;
-
-// process.env.PORT || 3000;
-const port = process.env.PORT || 3000;
+const { DB_HOST, PORT } = process.env;
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(port, () => {
-      console.log("Server running. Use our API on port: 3000");
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
     });
   })
   .catch((error) => console.log(error.message));
